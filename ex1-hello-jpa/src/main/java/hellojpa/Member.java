79a037f275550361eb95f5ae5efb4cfd77b55996
KIMTAEHYUN <th1993kim@gmail.com>
2022-04-17 18:39:33 +0900

    jpaShop

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,12 +1,14 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 //@Table(name="USER")
 @Entity
@@ -15,45 +17,90 @@ public class Member {
 	@Column(name = "MEMBER_ID")
 	private Long id;
 
-	@Column(name = "USERNAME")
-	private String username;
+	@Column(name = "NAME")
+	private String name;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "TEAM_ID")
-	private Team team;
+	private String city;
+	private String street;
+	private String zipcode;
+	
+	@OneToMany
+	@JoinColumn(name = "ORDER_ID")
+	private List<Order> orders = new ArrayList<Order>();
+	
+	
+	
+	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "TEAM_ID")
+//	private Team team;
 	
 //	@Column(name = "TEAM_ID")
 //	private Long teamId;
 
-	public Team getTeam() {
-		return team;
-	}
-
-	public void changeTeam(Team team) {
-		this.team = team;
-		team.getMembers().add(this);
-	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public Member() {
+	super();
+	}
+
+
+	public Member(Long id, String name, String city, String street, String zipcode, List<Order> orders) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+		this.orders = orders;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void setTeam(Team team) {
-		this.team = team;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setStreet(String street) {
+		this.street = street;
 	}
 
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
 
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
 	
 }
