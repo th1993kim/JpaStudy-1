9037641d1871952b115cd918f93f1d4eb85e8313
th1993kim <81105748+th1993kim@users.noreply.github.com>
2022-04-27 01:35:34 +0900

    trans

--- jpa-first-step/src/main/java/jpa/Child.java
+++ jpa-first-step/src/main/java/jpa/Child.java
@@ -1,46 +1,42 @@
 package jpa;
+
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Member {
+public class Child {
 
 	@Id @GeneratedValue
 	private Long id;
 	private String name;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="TEAM_ID")
-	private Team team;
-
+	@ManyToOne
+	@JoinColumn(name = "PARENT_ID")
+	private Parent parent;
+	
+	
+	
+	public Parent getParent() {
+		return parent;
+	}
+	public void setParent(Parent parent) {
+		this.parent = parent;
+	}
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public Team getTeam() {
-		return team;
-	}
-
-	public void setTeam(Team team) {
-		this.team = team;
-	}
-	
 	
 	
 }
--- jpa-first-step/src/main/java/jpa/JpaMain.java
+++ jpa-first-step/src/main/java/jpa/JpaMain.java
@@ -1,9 +1,13 @@
 package jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import javassist.expr.Instanceof;
 
 public class JpaMain {
 
@@ -15,14 +19,24 @@ public class JpaMain {
 		tx.begin();
 		
 		try {
-			Team team = new Team();
-			team.setName("Team1");
-			em.persist(team);
 			
-			Member member = new Member();
-			member.setName("Member1");
-			member.setTeam(team);
-			em.persist(member);
+			Child child1 = new Child();
+			Child child2 = new Child();
+			
+			Parent parent = new Parent();
+			
+			parent.addChild(child1);
+			
+			parent.addChild(child2);
+			
+			em.persist(parent);
+			
+			em.flush();
+			em.clear();
+			
+			Parent findParent = em.find(Parent.class, parent.getId());
+			findParent.getChildList().remove(0);
+			
 			
 			tx.commit();
 			
